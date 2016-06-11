5d689e3928e38128f9259dff5f9e2147962df814
Stephen Liang <stphnliang@gmail.com>
2016-06-11 12:07:30 -0700

    graph algorithms wk1

--- UCSD_AlgorithmsOnGraphs/09_graph_decomposition_starter_files_1/connected_components/connected_components.cpp
+++ UCSD_AlgorithmsOnGraphs/09_graph_decomposition_starter_files_1/connected_components/connected_components.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 
-using std::vector;
-using std::pair;
+using namespace std;
+
+// explore and mark all nodes reacheable from x
+void explore(vector<vector<int> > &adj, bool visited[], int x) {
+	stack<int> S;
+
+	S.push(x);
+
+	while (!S.empty()) {
+		int v = S.top();
+		S.pop();
+		if (!visited[v]) {
+			visited[v] = true;
+			for (int j = 0; j < adj[v].size(); j++) {
+				S.push(adj[v][j]);
+			}
+		}
+	}
+}
 
 int number_of_components(vector<vector<int> > &adj) {
-  int res = 0;
-  //write your code here
-  return res;
+	int res = 0;
+	size_t n = adj.size();
+	bool visited[n];
+
+	for (int i = 0; i < n; i++) {
+		visited[i] = false;
+	}
+
+	for (int i = 0; i < n; i ++) {
+		if (!visited[i]) {
+			explore(adj, visited, i);
+			res++;
+		}
+	}
+
+	return res;
 }
 
 int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << number_of_components(adj);
+	size_t n, m;
+	std::cin >> n >> m;
+	vector<vector<int> > adj(n, vector<int>());
+	for (size_t i = 0; i < m; i++) {
+		int x, y;
+		std::cin >> x >> y;
+		adj[x - 1].push_back(y - 1);
+		adj[y - 1].push_back(x - 1);
+	}
+	std::cout << number_of_components(adj);
 }
--- UCSD_AlgorithmsOnGraphs/09_graph_decomposition_starter_files_1/reachability/reachability.cpp
+++ UCSD_AlgorithmsOnGraphs/09_graph_decomposition_starter_files_1/reachability/reachability.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <stdint.h>
 
-using std::vector;
-using std::pair;
-
+using namespace std;
 
 int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+	//write your code here
+	size_t n = adj.size();
+	stack<int> S;
+	bool visited[n];
+	for (int i = 0; i < n; i++) {
+		visited[i] = false;
+	}
+	S.push(x);
+
+	while (!S.empty()) {
+		int v = S.top();
+		S.pop();
+		if (!visited[v]) {
+			visited[v] = true;
+			for (int j = 0; j < adj[v].size(); j++) {
+				S.push(adj[v][j]);
+			}
+		}
+	}
+
+	return (visited[y])? 1 : 0;
 }
 
 int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  int x, y;
-  std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+	size_t n, m;
+	std::cin >> n >> m; // n - number of vertices, m - number of edges
+	vector<vector<int> > adj(n, vector<int>());
+	for (size_t i = 0; i < m; i++) {
+		int x, y;
+		std::cin >> x >> y;
+		adj[x - 1].push_back(y - 1);
+		adj[y - 1].push_back(x - 1);
+	}
+	int x, y;
+	std::cin >> x >> y;
+	std::cout << reach(adj, x - 1, y - 1);
 }
