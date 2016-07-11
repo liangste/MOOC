c03ef5ca771c6f50dc3a58b9cd735645c4f31fb1
Stephen Liang <stphnliang@gmail.com>
2016-07-10 18:13:11 -0700

    need to rework rope implementation

--- UCSD_DataStructures/Starters PA4/rope/rope.cpp
+++ UCSD_DataStructures/Starters PA4/rope/rope.cpp
@@ -1,388 +1,265 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
-#include <math.h>
-#include <vector>
-
-// TODO run mega_large_test under 2.7 seconds
-
-#define DEBUG 0
 
 using namespace std;
 
-typedef struct rope_node rope;
 
-// invariants
-// either data is non-empty or left and right are both non-empty
-// left and right must both be set, in which case the rope_node is a
-// meta-node. Otherwise it is a data node
-struct rope_node {
-	int l_len; // length of string under left sub-tree
-	int len; // length of string under both left and right sub-trees
-	int height;
-	rope* left;
-	rope* right;
-	rope* parent;
-	string data;
+// Splay tree implementation
 
-	// construct data-rope_node
-	rope_node(const string& d)
-		: l_len(d.size())
-		, len(d.size())
-		, height(0)
-		, left(NULL)
-		, right(NULL)
-		, parent(NULL)
-		, data(d) {
-	};
+// Vertex of a splay tree
+struct Vertex {
+	int key;
+	// Sum of all the keys in the subtree - remember to update
+	// it after each operation that changes the tree.
+	long long sum;
+	Vertex* left;
+	Vertex* right;
+	Vertex* parent;
 
-	// construct meta-data-rope_node
-	rope_node(rope* ll, rope* rr)
-		: l_len(0)
-		, len(0)
-		, left(ll)
-		, right(rr)
-		, parent(NULL)
-		, data("") {
-
-		// update height
-		int left_height = 0;
-		int right_height = 0;
-
-		if (ll != NULL) {
-			l_len = ll->len;
-			len = ll->len;
-			ll->parent = this;
-			left_height = ll->height;
-		}
-		if (rr != NULL) {
-			len += rr->len;
-			rr->parent = this;
-			right_height = rr->height;
-		}
-
-		if (left_height > right_height) {
-			height = left_height + 1;
-		} else {
-			height = right_height + 1;
-		}
-	};
+	Vertex(int key, long long sum, Vertex* left, Vertex* right, Vertex* parent)
+	: key(key), sum(sum), left(left), right(right), parent(parent) {}
 };
 
-// create a rope object from string
-rope* ropify(const string& str);
+Vertex* g_root = NULL;
 
-// convert a rope to sring
-void stringfy(rope*& r, string& str);
-
-void report(rope*& r);
-
-// print out string of rope from pos i to j
-void report(rope*& r, int i, int j);
-
-// simplify a rope structure
-void reduce(rope*& r);
-
-// recursively update stats in a bottom-up manner
-void update_stats(rope*& r);
-
-void rotate_left(rope*& r);
-
-void rotate_right(rope*& r);
-
-// concatenates two ropes into one, and return pointer to the new rope
-rope* concat(rope*& left, rope*& right);
-
-// split an original rope into left and right ropes at position i, character
-// at position i goes to the newly formed right rope
-void split(rope*& original, int i, rope*& right);
-
-// insert substring at pos i
-void insert(rope*& target, rope*& sub, int i);
-
-rope* ropify(const string& str) {
-	int len = str.size();
-	int seg_len = (int) log2(len);
-	if (seg_len == 0) {
-		seg_len = 1;
-	}
-	int cur_idx = 0;
-	string str_seg = "";
-	rope* new_rope = NULL;
-	rope* root_rope = NULL;
-
-	while (cur_idx < len) {
-		str_seg = str.substr(cur_idx, seg_len);
-		new_rope = new rope(str_seg);
-		root_rope = concat(root_rope, new_rope);
-		cur_idx += seg_len;
-	}
-
-	return root_rope;
-}
-
-void stringfy(rope*& r, string& str) {
-	// do post-order traversal
-
-	if (r == NULL)
+void pre_traversal(Vertex*& root)
+{
+	Vertex* v = root;
+	if (v == NULL)
 		return;
 
-	stringfy(r->left, str);
-	stringfy(r->right, str);
-	str += r->data;
+	cout << "(" << v->key << "," << v->sum << ") " ;
+	pre_traversal(v->left);
+	pre_traversal(v->right);
 }
 
-void report_helper(rope*& r) {
-	if (r == NULL) {
-		cout << "NULL" << endl;
+void do_pre_traversal()
+{
+	cout << "doing pre-order traversal " << endl;
+	pre_traversal(g_root);
+	cout << "done" << endl;
+}
+
+void update(Vertex* v) {
+	if (v == NULL) return;
+	v->sum = v->key + (v->left != NULL ? v->left->sum : 0ll) + (v->right != NULL ? v->right->sum : 0ll);
+	if (v->left != NULL) {
+		v->left->parent = v;
+	}
+	if (v->right != NULL) {
+		v->right->parent = v;
+	}
+}
+
+void small_rotation(Vertex* v) {
+	Vertex* parent = v->parent;
+	if (parent == NULL) {
+		return;
+	}
+	Vertex* grandparent = v->parent->parent;
+	if (parent->left == v) {
+		Vertex* m = v->right;
+		v->right = parent;
+		parent->left = m;
 	} else {
-		cout << "(" << r->l_len << "," << r->len << ") " << r->data << endl;
-		cout << "left ";
-		report_helper(r->left);
-		cout << "right " ;
-		report_helper(r->right);
+		Vertex* m = v->left;
+		v->left = parent;
+		parent->right = m;
+	}
+
+	// update order statistics
+	update(parent);
+	update(v);
+	v->parent = grandparent;
+	if (grandparent != NULL) {
+		if (grandparent->left == parent) {
+			grandparent->left = v;
+		} else {
+			grandparent->right = v;
+		}
 	}
 }
 
-void report(rope*& r) {
-	string str = "";
-	stringfy(r, str);
-	cout << "Reporting string '" << str << "'" << endl;
-	//cout << "Printing nodes" << endl;
-	//report_helper(r);
+void big_rotation(Vertex* v) {
+	if (v->parent->left == v && v->parent->parent->left == v->parent) {
+		// Zig-zig
+		small_rotation(v->parent);
+		small_rotation(v);
+	} else if (v->parent->right == v && v->parent->parent->right == v->parent) {
+		// Zig-zig
+		small_rotation(v->parent);
+		small_rotation(v);
+	} else {
+		// Zig-zag
+		small_rotation(v);
+		small_rotation(v);
+	}
 }
 
-void reduce(rope*& r) {
-	if (r == NULL)
+// Makes splay of the given vertex and makes
+// it the new root.
+void splay(Vertex*& root, Vertex* v) {
+	if (v == NULL) return;
+	while (v->parent != NULL) {
+		if (v->parent->parent == NULL) {
+			small_rotation(v);
+			break;
+		}
+		big_rotation(v);
+	}
+	root = v;
+}
+
+// Searches for the given key in the tree with the given root
+// and calls splay for the deepest visited node after that.
+// If found, returns a pointer to the node with the given key.
+// Otherwise, returns a pointer to the node with the smallest
+// bigger key (next value in the order).
+// If the key is bigger than all keys in the tree,
+// returns NULL.
+Vertex* find(Vertex*& root, int key) {
+	Vertex* v = root;
+	Vertex* last = root;
+	Vertex* next = NULL;
+	while (v != NULL) {
+		if (v->key >= key && (next == NULL || v->key < next->key)) {
+			next = v;
+		}
+		last = v;
+		if (v->key == key) {
+			break;
+		}
+		if (v->key < key) {
+			v = v->right;
+		} else {
+			v = v->left;
+		}
+	}
+	splay(root, last);
+	return next;
+}
+
+Vertex* left_descendent(Vertex*& node) {
+	if (node->left == NULL)
+		return node;
+	else
+		return left_descendent(node->left);
+}
+
+Vertex* right_ancestor(Vertex*& node) {
+	if (node->parent == NULL)
+		return NULL;
+
+	if (node->key < node->parent->key)
+		return node->parent;
+	else
+		return right_ancestor(node->parent);
+}
+
+// return the next node, or NULL if no such node exists
+Vertex* get_next(Vertex*& node) {
+	if (node->right != NULL)
+		return left_descendent(node->right);
+	else
+		return right_ancestor(node);
+}
+
+void split(Vertex* root, int key, Vertex*& left, Vertex*& right) {
+	right = find(root, key);
+	splay(root, right);
+	if (right == NULL) {
+		left = root;
 		return;
-	reduce(r->left);
-	reduce(r->right);
-	if (r->right == NULL && r->left == NULL && r->data.size() == 0)
-	{
-		delete r;
-		r = NULL;
 	}
+	left = right->left;
+	right->left = NULL;
+	if (left != NULL) {
+		left->parent = NULL;
+	}
+	update(left);
+	update(right);
 }
 
-void update_stats(rope*& r) {
-	if (r == NULL) {
-		return;
-	}
-	if (r->left != NULL) {
-		r->l_len = r->left->len;
-		r->len = r->left->len;
-	}
-	if (r->right != NULL) {
-		r->len += r->right->len;
-	}
-	update_stats(r->parent);
-}
-
-rope* concat(rope*& left, rope*& right) {
+Vertex* merge(Vertex* left, Vertex* right) {
 	if (left == NULL) return right;
 	if (right == NULL) return left;
-	rope* new_rope = new rope(left, right);
-	reduce(new_rope);
-	return new_rope;
+	Vertex* min_right = right;
+	while (min_right->left != NULL) {
+		min_right = min_right->left;
+	}
+	splay(right, min_right);
+	right->left = left;
+	update(right);
+	return right;
 }
 
-void split_helper(rope*& root, int i, rope*& right) {
-	if (NULL == root) {
-		cout << "Cannot split string that is NULL" << endl;
+void promote(Vertex*& old, Vertex*& newer) {
+	Vertex* oldold = old;
+	old->key = newer->key;
+	old->sum = newer->sum;
+	old->left = newer->left;
+	old->right = newer->right;
+}
+
+// assume node is at root already
+void delete_vertex(Vertex*& node) {
+	if (node->parent) {
+		cout << "SOMETHING IS WRONG, deleting a vertex that still has parent" << endl;
 		return;
 	}
 
-	if (i >= root->len || i < 0) {
-		//cout << "Cannot split string with length " << root->len << " at pos " << i << endl;
-		return;
-	}
+	Vertex* promoted = NULL;
 
-	if (i <= root->l_len) {
-		if (root->left == NULL && root->right == NULL) {
-			// this is the case where we must split a data node into 2
-			if (i >= root->data.size()) {
-				cout << "Something is very wrong" << endl;
-				return;
-			}
-			string split_right = root->data.substr(i);
-			root->data = root->data.substr(0, i);
-			root->l_len = root->len = root->data.size();
-			rope* new_rope = new rope(split_right);
-			right = concat(new_rope, right);
-
-			// TODO need to update tree stats
-			update_stats(root);
-		} else {
-			right = concat(root->right, right);
-			root->right = NULL;
-			split_helper(root->left, i, right);
-
-			// TODO need to update tree stats
-			update_stats(root);
+	if (node->right == NULL) {
+		if (node->left !=NULL) {
+			node->left->parent = NULL;
 		}
+		g_root = node->left;
 	} else {
-		split_helper(root->right, i - root->l_len, right);
-	}
-}
-
-void split(rope*& root, int i, rope*& right) {
-	split_helper(root, i, right);
-	reduce(root);
-	reduce(right);
-}
-
-void insert(rope*& target, rope*& sub, int i) {
-	rope* right = NULL;
-	split(target, i , right);
-	target = concat(target, sub);
-	target = concat(target, right);
-}
-
-void rope_dev_test() {
-	cout << "Starting rope development test" << endl;
-
-	// test concat()
-	rope* hello = new rope("hello_");
-	rope* my = new rope("my_");
-	rope* name = new rope("name_");
-	rope* is_simon = new rope("is_Simon");
-	hello = concat(hello, my);
-	hello = concat(hello, name);
-	hello = concat(hello, is_simon);
-	//report(hello);
-
-	// test split()
-	rope* e = new rope("Hello_");
-	rope* f = new rope("my_");
-	rope* j = new rope("na");
-	rope* k = new rope("me_i");
-	rope* m = new rope("s");
-	rope* n = new rope("_Simon");
-	rope* c = new rope(e, f);
-	rope* g = new rope(j, k);
-	rope* h = new rope(m, n);
-	rope* d = new rope(g, h);
-	rope* b = new rope(c, d);
-	rope* a = new rope(b, NULL);
-
-	string expected_string, left_string, right_string, result_string;
-	stringfy(a, expected_string);
-	rope* right;
-
-	report(a);
-	rope* n_o_t = new rope("_not");
-	insert(a, n_o_t, 16);
-	report(a);
-
-	string user_name = "stephen liang";
-	rope* my_name = ropify(user_name);
-	report(my_name);
-
-	/*
-	for (int i = 0; i <= expected_string.size(); i++) { // should be no problem specifying bad offsets
-		right = NULL;
-		split(a, i, right);
-		left_string = "";
-		right_string = "";
-		result_string = "";
-		stringfy(a, left_string);
-		stringfy(right, right_string);
-		result_string = left_string + right_string;
-
-		if (result_string != expected_string) {
-			cout << "Failed, actual string is '" << result_string << "'" << endl;
-			return;
-		} else {
-			cout << "Passed splitting at pos " << i << endl;
+		promoted = get_next(node);
+		promoted->left = node->left;
+		if (node->left != NULL) {
+			node->left->parent = promoted;
 		}
-		a = concat(a, right);
+		//splay(g_root, promoted);
+		promoted->parent = NULL;
+		g_root = promoted;
 	}
-
-	report(a);
-	*/
+	update(g_root);
 }
 
-class RopeTest {
-	rope* root_rope;
-	string s;
-	bool naive_used;
+class Rope {
+	std::string s;
 public:
-	RopeTest(const std::string &s)
-		: root_rope(NULL)
-		, s(s)
-		, naive_used(false) {
-			root_rope = ropify(s);
+	Rope(const std::string &s) : s(s) {
 	}
 
 	void process_naive( int i, int j, int k ) {
-		naive_used = true;
 		// Replace this code with a faster implementation
 		std::string t = s.substr(0, i) + s.substr(j + 1);
 		s = t.substr(0, k) + s.substr(i, j - i + 1) + t.substr(k);
 	}
 
 	void process( int i, int j, int k ) {
-		rope* left = root_rope;
-		rope* middle = NULL;
-		rope* right = NULL;
-
-		if (i >= root_rope->len || i < 0) {
-			return;
-		}
-
-		if (j < i) {
-			return;
-		}
-
-		if (j > root_rope->len) {
-			return;
-		}
-
-		if (i == k) {
-			return;
-		}
-
-		if (k < 0) {
-			return;
-		}
-
-		int extracted_length = root_rope->len - (j - i + 1);
-		if (k > extracted_length) {
-			return;
-		}
-
-		split(left, i, middle);
-		split(middle, j - i + 1, right);
-		left = concat(left, right);
-		insert(left, middle, k);
-		root_rope = left;
 	}
 
 	std::string result() {
-		if (naive_used)
-			return s;
-
-		string ret = "";
-		stringfy(root_rope, ret);
-		return ret;
+		return s;
 	}
 };
 
 int main() {
-#if DEBUG
-	rope_dev_test();
-	return 0;
-#endif
 	std::ios_base::sync_with_stdio(0);
 	std::string s;
 	std::cin >> s;
-	RopeTest ropeTest(s);
+	Rope rope(s);
 	int actions;
 	std::cin >> actions;
-	for (int action_index = 0; action_index < actions; ++action_index) {
-		int i, j, k;
+				for (int action_index = 0; action_index < actions; ++action_index) {
+								int i, j, k;
 		std::cin >> i >> j >> k;
-		ropeTest.process(i, j, k);
+		rope.process(i, j, k);
 	}
-	std::cout << ropeTest.result() << std::endl;
+	std::cout << rope.result() << std::endl;
 }
--- UCSD_DataStructures/Starters PA4/set_range_sum/set_range_sum.cpp
+++ UCSD_DataStructures/Starters PA4/set_range_sum/set_range_sum.cpp
@@ -9,106 +9,106 @@ using namespace std;
 
 // Vertex of a splay tree
 struct Vertex {
-  int key;
-  // Sum of all the keys in the subtree - remember to update
-  // it after each operation that changes the tree.
-  long long sum;
-  Vertex* left;
-  Vertex* right;
-  Vertex* parent;
+	int key;
+	// Sum of all the keys in the subtree - remember to update
+	// it after each operation that changes the tree.
+	long long sum;
+	Vertex* left;
+	Vertex* right;
+	Vertex* parent;
 
-  Vertex(int key, long long sum, Vertex* left, Vertex* right, Vertex* parent)
-  : key(key), sum(sum), left(left), right(right), parent(parent) {}
+	Vertex(int key, long long sum, Vertex* left, Vertex* right, Vertex* parent)
+	: key(key), sum(sum), left(left), right(right), parent(parent) {}
 };
 
 Vertex* g_root = NULL;
 
 void pre_traversal(Vertex*& root)
 {
-  Vertex* v = root;
-  if (v == NULL)
-    return;
+	Vertex* v = root;
+	if (v == NULL)
+		return;
 
-  cout << "(" << v->key << "," << v->sum << ") " ;
-  pre_traversal(v->left);
-  pre_traversal(v->right);
+	cout << "(" << v->key << "," << v->sum << ") " ;
+	pre_traversal(v->left);
+	pre_traversal(v->right);
 }
 
 void do_pre_traversal()
 {
-  cout << "doing pre-order traversal " << endl;
-  pre_traversal(g_root);
-  cout << "done" << endl;
+	cout << "doing pre-order traversal " << endl;
+	pre_traversal(g_root);
+	cout << "done" << endl;
 }
 
 void update(Vertex* v) {
-  if (v == NULL) return;
-  v->sum = v->key + (v->left != NULL ? v->left->sum : 0ll) + (v->right != NULL ? v->right->sum : 0ll);
-  if (v->left != NULL) {
-    v->left->parent = v;
-  }
-  if (v->right != NULL) {
-    v->right->parent = v;
-  }
+	if (v == NULL) return;
+	v->sum = v->key + (v->left != NULL ? v->left->sum : 0ll) + (v->right != NULL ? v->right->sum : 0ll);
+	if (v->left != NULL) {
+		v->left->parent = v;
+	}
+	if (v->right != NULL) {
+		v->right->parent = v;
+	}
 }
 
 void small_rotation(Vertex* v) {
-  Vertex* parent = v->parent;
-  if (parent == NULL) {
-    return;
-  }
-  Vertex* grandparent = v->parent->parent;
-  if (parent->left == v) {
-    Vertex* m = v->right;
-    v->right = parent;
-    parent->left = m;
-  } else {
-    Vertex* m = v->left;
-    v->left = parent;
-    parent->right = m;
-  }
+	Vertex* parent = v->parent;
+	if (parent == NULL) {
+		return;
+	}
+	Vertex* grandparent = v->parent->parent;
+	if (parent->left == v) {
+		Vertex* m = v->right;
+		v->right = parent;
+		parent->left = m;
+	} else {
+		Vertex* m = v->left;
+		v->left = parent;
+		parent->right = m;
+	}
 
-  // update order statistics
-  update(parent);
-  update(v);
-  v->parent = grandparent;
-  if (grandparent != NULL) {
-    if (grandparent->left == parent) {
-      grandparent->left = v;
-    } else {
-      grandparent->right = v;
-    }
-  }
+	// update order statistics
+	update(parent);
+	update(v);
+	v->parent = grandparent;
+	if (grandparent != NULL) {
+		if (grandparent->left == parent) {
+			grandparent->left = v;
+		} else {
+			grandparent->right = v;
+		}
+	}
 }
 
 void big_rotation(Vertex* v) {
-  if (v->parent->left == v && v->parent->parent->left == v->parent) {
-    // Zig-zig
-    small_rotation(v->parent);
-    small_rotation(v);
-  } else if (v->parent->right == v && v->parent->parent->right == v->parent) {
-    // Zig-zig
-    small_rotation(v->parent);
-    small_rotation(v);
-  } else {
-    // Zig-zag
-    small_rotation(v);
-    small_rotation(v);
-  }
+	if (v->parent->left == v && v->parent->parent->left == v->parent) {
+		// Zig-zig
+		small_rotation(v->parent);
+		small_rotation(v);
+	} else if (v->parent->right == v && v->parent->parent->right == v->parent) {
+		// Zig-zig
+		small_rotation(v->parent);
+		small_rotation(v);
+	} else {
+		// Zig-zag
+		small_rotation(v);
+		small_rotation(v);
+	}
 }
 
 // Makes splay of the given vertex and makes
 // it the new root.
 void splay(Vertex*& root, Vertex* v) {
-  if (v == NULL) return;
-  while (v->parent != NULL) {
-    if (v->parent->parent == NULL) {
-      small_rotation(v);
-      break;
-    }
-    big_rotation(v);
-  }
-  root = v;
+	if (v == NULL) return;
+	while (v->parent != NULL) {
+		if (v->parent->parent == NULL) {
+			small_rotation(v);
+			break;
+		}
+		big_rotation(v);
+	}
+	root = v;
 }
 
 // Searches for the given key in the tree with the given root
@@ -119,228 +119,228 @@ void splay(Vertex*& root, Vertex* v) {
 // If the key is bigger than all keys in the tree,
 // returns NULL.
 Vertex* find(Vertex*& root, int key) {
-  Vertex* v = root;
-  Vertex* last = root;
-  Vertex* next = NULL;
-  while (v != NULL) {
-    if (v->key >= key && (next == NULL || v->key < next->key)) {
-      next = v;
-    }
-    last = v;
-    if (v->key == key) {
-      break;
-    }
-    if (v->key < key) {
-      v = v->right;
-    } else {
-      v = v->left;
-    }
-  }
-  splay(root, last);
-  return next;
+	Vertex* v = root;
+	Vertex* last = root;
+	Vertex* next = NULL;
+	while (v != NULL) {
+		if (v->key >= key && (next == NULL || v->key < next->key)) {
+			next = v;
+		}
+		last = v;
+		if (v->key == key) {
+			break;
+		}
+		if (v->key < key) {
+			v = v->right;
+		} else {
+			v = v->left;
+		}
+	}
+	splay(root, last);
+	return next;
 }
 
 Vertex* left_descendent(Vertex*& node) {
-  if (node->left == NULL)
-    return node;
-  else
-    return left_descendent(node->left);
+	if (node->left == NULL)
+		return node;
+	else
+		return left_descendent(node->left);
 }
 
 Vertex* right_ancestor(Vertex*& node) {
-  if (node->parent == NULL)
-    return NULL;
+	if (node->parent == NULL)
+		return NULL;
 
-  if (node->key < node->parent->key)
-    return node->parent;
-  else
-    return right_ancestor(node->parent);
+	if (node->key < node->parent->key)
+		return node->parent;
+	else
+		return right_ancestor(node->parent);
 }
 
 // return the next node, or NULL if no such node exists
 Vertex* get_next(Vertex*& node) {
-  if (node->right != NULL)
-    return left_descendent(node->right);
-  else
-    return right_ancestor(node);
+	if (node->right != NULL)
+		return left_descendent(node->right);
+	else
+		return right_ancestor(node);
 }
 
 void split(Vertex* root, int key, Vertex*& left, Vertex*& right) {
-  right = find(root, key);
-  splay(root, right);
-  if (right == NULL) {
-    left = root;
-    return;
-  }
-  left = right->left;
-  right->left = NULL;
-  if (left != NULL) {
-    left->parent = NULL;
-  }
-  update(left);
-  update(right);
+	right = find(root, key);
+	splay(root, right);
+	if (right == NULL) {
+		left = root;
+		return;
+	}
+	left = right->left;
+	right->left = NULL;
+	if (left != NULL) {
+		left->parent = NULL;
+	}
+	update(left);
+	update(right);
 }
 
 Vertex* merge(Vertex* left, Vertex* right) {
-  if (left == NULL) return right;
-  if (right == NULL) return left;
-  Vertex* min_right = right;
-  while (min_right->left != NULL) {
-    min_right = min_right->left;
-  }
-  splay(right, min_right);
-  right->left = left;
-  update(right);
-  return right;
+	if (left == NULL) return right;
+	if (right == NULL) return left;
+	Vertex* min_right = right;
+	while (min_right->left != NULL) {
+		min_right = min_right->left;
+	}
+	splay(right, min_right);
+	right->left = left;
+	update(right);
+	return right;
 }
 
 void promote(Vertex*& old, Vertex*& newer) {
-  Vertex* oldold = old;
-  old->key = newer->key;
-  old->sum = newer->sum;
-  old->left = newer->left;
-  old->right = newer->right;
+	Vertex* oldold = old;
+	old->key = newer->key;
+	old->sum = newer->sum;
+	old->left = newer->left;
+	old->right = newer->right;
 }
 
 // assume node is at root already
 void delete_vertex(Vertex*& node) {
-  if (node->parent) {
-    cout << "SOMETHING IS WRONG, deleting a vertex that still has parent" << endl;
-    return;
-  }
+	if (node->parent) {
+		cout << "SOMETHING IS WRONG, deleting a vertex that still has parent" << endl;
+		return;
+	}
 
-  Vertex* promoted = NULL;
+	Vertex* promoted = NULL;
 
-  if (node->right == NULL) {
-    if (node->left !=NULL) {
-      node->left->parent = NULL;
-    }
-    g_root = node->left;
-  } else {
-    promoted = get_next(node);
-    promoted->left = node->left;
-    if (node->left != NULL) {
-      node->left->parent = promoted;
-    }
-    //splay(g_root, promoted);
-    promoted->parent = NULL;
-    g_root = promoted;
-  }
-  update(g_root);
+	if (node->right == NULL) {
+		if (node->left !=NULL) {
+			node->left->parent = NULL;
+		}
+		g_root = node->left;
+	} else {
+		promoted = get_next(node);
+		promoted->left = node->left;
+		if (node->left != NULL) {
+			node->left->parent = promoted;
+		}
+		//splay(g_root, promoted);
+		promoted->parent = NULL;
+		g_root = promoted;
+	}
+	update(g_root);
 }
 
 // Code that uses splay tree to solve the problem
 
 void insert(int x) {
 #if DEBUG
-  cout << "### add " << x << endl;
+	cout << "### add " << x << endl;
 #endif
-  Vertex* left = NULL;
-  Vertex* right = NULL;
-  Vertex* new_vertex = NULL;
-  split(g_root, x, left, right);
-  if (right == NULL || right->key != x) {
-    new_vertex = new Vertex(x, x, NULL, NULL, NULL);
-  }
-  g_root = merge(merge(left, new_vertex), right);
+	Vertex* left = NULL;
+	Vertex* right = NULL;
+	Vertex* new_vertex = NULL;
+	split(g_root, x, left, right);
+	if (right == NULL || right->key != x) {
+		new_vertex = new Vertex(x, x, NULL, NULL, NULL);
+	}
+	g_root = merge(merge(left, new_vertex), right);
 }
 
 void erase(int x) {
 #if DEBUG
-  cout << "### del " << x << endl;
+	cout << "### del " << x << endl;
 #endif
-  // Implement erase yourself
-  Vertex* target = find(g_root, x);
-  Vertex* next = NULL;
+	// Implement erase yourself
+	Vertex* target = find(g_root, x);
+	Vertex* next = NULL;
 
-  if (target == NULL) {
-    return;
-  }
-  if (target->key != x) {
-    return;
-  }
+	if (target == NULL) {
+		return;
+	}
+	if (target->key != x) {
+		return;
+	}
 
-  next = get_next(target);
-  splay(g_root, next);
-  splay(g_root, target);
-  delete_vertex(target);
+	next = get_next(target);
+	splay(g_root, next);
+	splay(g_root, target);
+	delete_vertex(target);
 }
 
 bool find(int x) {
 #if DEBUG
-  cout << "### find " << x << endl;
+	cout << "### find " << x << endl;
 #endif
-  // Implement find yourself
+	// Implement find yourself
 
-  Vertex* ret = find(g_root, x);
+	Vertex* ret = find(g_root, x);
 
-  if (ret == NULL)
-    return false;
+	if (ret == NULL)
+		return false;
 
-  return ret->key == x;
+	return ret->key == x;
 }
 
 long long sum(int from, int to) {
 #if DEBUG
-  cout << "### sum from " << from << " to " << to << endl;
+	cout << "### sum from " << from << " to " << to << endl;
 #endif
-  Vertex* left = NULL;
-  Vertex* middle = NULL;
-  Vertex* right = NULL;
-  split(g_root, from, left, middle);
-  split(middle, to + 1, middle, right);
-  long long ans = 0ll;
-  // Complete the implementation of sum
+	Vertex* left = NULL;
+	Vertex* middle = NULL;
+	Vertex* right = NULL;
+	split(g_root, from, left, middle);
+	split(middle, to + 1, middle, right);
+	long long ans = 0ll;
+	// Complete the implementation of sum
 
-  // sum all elements in middle
-  if (middle != NULL) {
-    update(middle);
-    ans = middle->sum;
-  }
+	// sum all elements in middle
+	if (middle != NULL) {
+		update(middle);
+		ans = middle->sum;
+	}
 
-  g_root = merge(merge(left, middle), right);
+	g_root = merge(merge(left, middle), right);
 
-  return ans;
+	return ans;
 }
 
 const int MODULO = 1000000001;
 
 int main(){
-  int n;
-  scanf("%d", &n);
-  int last_sum_result = 0;
-  for (int i = 0; i < n; i++) {
-    char buffer[10];
-    scanf("%s", buffer);
-    char type = buffer[0];
-    switch (type) {
-      case '+' : {
-        int x;
-        scanf("%d", &x);
-        insert((x + last_sum_result) % MODULO);
-      } break;
-      case '-' : {
-        int x;
-        scanf("%d", &x);
-        erase((x + last_sum_result) % MODULO);
-      } break;
-      case '?' : {
-        int x;
-        scanf("%d", &x);
-        printf(find((x + last_sum_result) % MODULO) ? "Found\n" : "Not found\n");
-      } break;
-      case 's' : {
-        int l, r;
-        scanf("%d %d", &l, &r);
-        long long res = sum((l + last_sum_result) % MODULO, (r + last_sum_result) % MODULO);
-        printf("%lld\n", res);
-        last_sum_result = int(res % MODULO);
-      }
-    }
+	int n;
+	scanf("%d", &n);
+	int last_sum_result = 0;
+	for (int i = 0; i < n; i++) {
+		char buffer[10];
+		scanf("%s", buffer);
+		char type = buffer[0];
+		switch (type) {
+			case '+' : {
+				int x;
+				scanf("%d", &x);
+				insert((x + last_sum_result) % MODULO);
+			} break;
+			case '-' : {
+				int x;
+				scanf("%d", &x);
+				erase((x + last_sum_result) % MODULO);
+			} break;
+			case '?' : {
+				int x;
+				scanf("%d", &x);
+				printf(find((x + last_sum_result) % MODULO) ? "Found\n" : "Not found\n");
+			} break;
+			case 's' : {
+				int l, r;
+				scanf("%d %d", &l, &r);
+				long long res = sum((l + last_sum_result) % MODULO, (r + last_sum_result) % MODULO);
+				printf("%lld\n", res);
+				last_sum_result = int(res % MODULO);
+			}
+		}
 
 #if DEBUG
-    do_pre_traversal();
+		do_pre_traversal();
 #endif
-  }
-  return 0;
+	}
+	return 0;
 }
