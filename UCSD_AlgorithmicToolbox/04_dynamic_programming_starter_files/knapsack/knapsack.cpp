a61c4da989357a6c2141624091f0795216517195
Stephen Liang <stphnliang@gmail.com>
2016-07-15 19:43:53 -0700

    unknown signals

--- UCSD_AlgorithmicToolbox/04_dynamic_programming_starter_files/knapsack/knapsack.cpp
+++ UCSD_AlgorithmicToolbox/04_dynamic_programming_starter_files/knapsack/knapsack.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <string.h>
 
+using namespace std;
 using std::vector;
 
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
-    }
-  }
-  return current_weight;
+	//write your code here
+	int value[W + 1][w.size() + 1];
+
+	memset(value, 0, sizeof(value));
+
+	for (int i = 1; i <= w.size(); i++) {
+		for (int weight = 1; weight <= W; weight++) {
+			value[weight][i] = value[weight][i - 1];
+			int cur_value = value[weight][i];
+			int cur_weight = w[i - 1];
+			if (cur_weight <= weight) { // if item i fits, we consider it
+				int new_value = value[weight - cur_weight][i - 1] + cur_weight;
+				if (new_value > cur_value) {
+					value[weight][i] = new_value;
+				}
+			}
+		}
+	}
+
+	/*
+	for (int w_i = 0; w_i <= w.size(); w_i++) {
+		cout << "i = " << w_i << " : ";
+		for (int weight = 0; weight <= W; weight++) {
+			cout << value[weight][w_i] << " ";
+		}
+		cout << endl;
+	}
+	*/
+
+	return value[W][w.size()];
 }
 
 int main() {
-  int n, W;
-  std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
-  std::cout << optimal_weight(W, w) << '\n';
+	int n, W;
+	std::cin >> W >> n;
+	vector<int> w(n);
+	for (int i = 0; i < n; i++) {
+		std::cin >> w[i];
+	}
+	std::cout << optimal_weight(W, w) << '\n';
 }
--- UCSD_AlgorithmicToolbox/04_dynamic_programming_starter_files/primitive_calculator/primitive_calculator.cpp
+++ UCSD_AlgorithmicToolbox/04_dynamic_programming_starter_files/primitive_calculator/primitive_calculator.cpp
@@ -1,31 +1,87 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
+using namespace std;
 using std::vector;
 
+enum op_type {
+	OpAdd1 = 0,
+	OpMult2,
+	OpMult3
+};
+
 vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
-    }
-  }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
+	std::vector<int> sequence; // 0 to n
+	std::vector<int> cost(n + 1, 0);
+	std::vector<op_type> ops(n + 1, OpAdd1);
+
+	for (int i = 2; i <= n; i++) {
+		enum op_type min_cost_op = OpAdd1;
+		int op1_cost = cost[i - 1] + 1;
+		int op2_cost = INT_MAX;
+		int op3_cost = INT_MAX;
+		if ((i % 2) == 0) {
+			op2_cost = cost[i/2] + 1;
+		}
+		if ((i % 3) == 0) {
+			op3_cost = cost[i/3] + 1;
+		}
+
+		if (op1_cost > op2_cost) {
+			if (op2_cost > op3_cost) {
+				min_cost_op = OpMult3;
+			} else {
+				min_cost_op = OpMult2;
+			}
+		} else {
+			if (op1_cost > op3_cost) {
+				min_cost_op = OpMult3;
+			} else {
+				min_cost_op = OpAdd1;
+			}
+		}
+
+		switch (min_cost_op) {
+			case OpAdd1:
+				cost[i] = op1_cost;
+				break;
+			case OpMult2:
+				cost[i] = op2_cost;
+				break;
+			case OpMult3:
+			default:
+				cost[i] = op3_cost;
+				break;
+		}
+
+		ops[i] = min_cost_op;
+	}
+
+	int elem = n;
+	while (elem > 0) {
+		sequence.push_back(elem);
+		if (ops[elem] == OpAdd1) {
+			elem -= 1;
+		}
+		else if (ops[elem] == OpMult2) {
+			elem /= 2;
+		}
+		else {
+			elem /= 3;
+		}
+	}
+	reverse(sequence.begin(), sequence.end());
+	return sequence;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
-    std::cout << sequence[i] << " ";
-  }
+	int n;
+	std::cin >> n;
+	vector<int> sequence = optimal_sequence(n);
+	std::cout << sequence.size() - 1 << std::endl;
+	for (size_t i = 0; i < sequence.size(); ++i) {
+		std::cout << sequence[i] << " ";
+	}
 }
