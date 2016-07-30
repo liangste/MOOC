2a137b78493c36f82d3ad7a80eab79864f9997e1
Stephen Liang <stphnliang@gmail.com>
2016-07-29 22:49:00 -0700

    wip

--- UCSD_AlgorithmsOnStrings/Programming-Assignment-1/trie_matching/trie_matching.cpp
+++ UCSD_AlgorithmsOnStrings/Programming-Assignment-1/trie_matching/trie_matching.cpp
@@ -10,17 +10,12 @@ using namespace std;
 int const Letters =    4;
 int const NA      =   -1;
 
-struct Node
-{
-	int next [Letters];
+struct Node {
+	int next[Letters];
 
-	Node ()
-	{
-		fill (next, next + Letters, NA);
-	}
+	Node() {fill(next, next + Letters, NA);}
 
-	bool isLeaf () const
-	{
+	bool isLeaf() const {
 	    return (next[0] == NA && next[1] == NA && next[2] == NA && next[3] == NA);
 	}
 };
@@ -37,31 +32,77 @@ int letterToIndex (char letter)
 	}
 }
 
-vector <int> solve (string text, int n, vector <string> patterns)
-{
-	vector <int> result;
+vector<int> solve(string text, int n, vector <string> patterns) {
+	vector<Node> trie;
+	vector<int> result;
 
-	// write your code here
+	Node root;
+	trie.push_back(root);
+	int currentNodeId;
+
+	// make a trie structure from suffixes to text
+	for (int i = 0; i < text.length(); i++) {
+		string suffix = text.substr(i);
+		currentNodeId = 0;
+		cout << "suffix = " << suffix << endl;
+		for (int j = 0; j < suffix.length(); j++) {
+			char currentSymbol = suffix[j];
+			if (trie[currentNodeId].isLeaf()) {
+				Node newNode;
+        trie[currentNodeId].next[letterToIndex(currentSymbol)] = trie.size();
+        trie.push_back(newNode);
+        currentNodeId = trie.size() - 1;
+      } else {
+				currentNodeId = trie[currentNodeId].next[letterToIndex(currentSymbol)];
+      }
+		}
+	}
+
+	cout << "constucted trie vector of size = " << trie.size() << endl;
+
+	// for each pattern in patterns, traverse the trie
+	unsigned searchIndex;
+	for (auto& p : patterns) {
+		searchIndex = 0;
+		bool inTrie = true;
+		for (unsigned u = 0; u < p.size(); u++) {
+			cout << "looking for " << p[u] << endl;
+			if (trie[searchIndex].next[letterToIndex(p[u])] != NA) {
+				cout << "1" << endl;
+				searchIndex = trie[searchIndex].next[letterToIndex(p[u])];
+			} else {
+				inTrie = false;
+			}
+			if (u == (p.size() - 1) && inTrie) {
+				cout << "found" << endl;
+			}
+		}
+	}
+	searchIndex = 0;
+	cout << "next[A] = " << trie[searchIndex].next[0] << endl;
+	cout << "next[C] = " << trie[searchIndex].next[1] << endl;
+	cout << "next[G] = " << trie[searchIndex].next[2] << endl;
+	cout << "next[T] = " << trie[searchIndex].next[3] << endl;
 
 	return result;
 }
 
 int main (void)
 {
-	string t;
+	string text;
 	cin >> text;
 
 	int n;
 	cin >> n;
 
-	vector <string> patterns (n);
+	vector<string> patterns(n);
 	for (int i = 0; i < n; i++)
 	{
 		cin >> patterns[i];
 	}
 
 	vector <int> ans;
-	ans = solve (t, n, s);
+	ans = solve(text, n, patterns);
 
 	for (int i = 0; i < (int) ans.size (); i++)
 	{
