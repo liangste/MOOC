a1e5ac8e35d2437265d4baeb3c8c9b014b2b3721
Stephen Liang <stphnliang@gmail.com>
2016-08-01 21:51:14 -0700

    Burrows-Wheeler Transform

--- UCSD_AlgorithmsOnStrings/Programming-Assignment-2/bwt/bwt.cpp
+++ UCSD_AlgorithmsOnStrings/Programming-Assignment-2/bwt/bwt.cpp
@@ -10,16 +10,27 @@ using std::string;
 using std::vector;
 
 string BWT(const string& text) {
-  string result = "";
+	string result = "";
+	vector<string> rotations;
 
-  // write your code here
+	// write your code here
+	for (unsigned u = 0; u < text.size(); u++) {
+		string chopped = text.substr(0, u);
+		string remaining = text.substr(u);
+		rotations.push_back(remaining + chopped);
+	}
+	sort(rotations.begin(), rotations.end());
 
-  return result;
+	for (string& r : rotations) {
+		result += r[r.size() - 1];
+	}
+
+	return result;
 }
 
 int main() {
-  string text;
-  cin >> text;
-  cout << BWT(text) << endl;
-  return 0;
+	string text;
+	cin >> text;
+	cout << BWT(text) << endl;
+	return 0;
 }
