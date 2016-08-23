0711cdbcf1bb4cf7171a41541b0fecf85c095b6e
Stephen Liang <stphnliang@gmail.com>
2016-08-22 21:50:42 -0700

    suffix_array_long

--- UCSD_AlgorithmsOnStrings/Programming-Assignment-3/suffix_array_long/suffix_array_long.cpp
+++ UCSD_AlgorithmsOnStrings/Programming-Assignment-3/suffix_array_long/suffix_array_long.cpp
@@ -1,26 +1,149 @@
+// works only with letters A, C, G, T, and $
+
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <utility>
+#include <map>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::make_pair;
-using std::pair;
-using std::string;
-using std::vector;
+using namespace std;
 
-// Build suffix array of the string text and
-// return a vector result of the same length as the text
-// such that the value result[i] is the index (0-based)
-// in text where the i-th lexicographically smallest
-// suffix of text starts.
+#define DEBUG 1
+
+void SortCharacters(const std::string& S, vector<int>& order) {
+  order.resize(S.size());
+  map<char, int> count;
+
+  count['$'] = 0;
+  count['A'] = 0;
+  count['C'] = 0;
+  count['G'] = 0;
+  count['T'] = 0;
+
+  for (int i = 0; i < S.size(); i++) {
+    count[S[i]]++;
+  }
+
+  count['A'] += count ['$'];
+  count['C'] += count ['A'];
+  count['G'] += count ['C'];
+  count['T'] += count ['G'];
+
+  for (int i = S.size() - 1; i >= 0; i--) {
+    count[S[i]]--;
+    order[count[S[i]]] = i;
+  }
+}
+
+void ComputeClassOrder(const std::string& S, const vector<int>& order, vector<int>& Class) {
+  Class.resize(S.size());
+  Class[order[0]] = 0;
+  for (int i = 1; i < S.size(); i++) {
+    if (S[order[i]] != S[order[i - 1]]) {
+      Class[order[i]] = Class[order[i - 1]] + 1;
+    } else {
+      Class[order[i]] = Class[order[i - 1]];
+    }
+  }
+}
+
+vector<int> SortDoubled(const std::string& S, int L, const vector<int>& order, vector<int>& Class) {
+  vector<int> count(S.size(), 0);
+  vector<int> newOrder(S.size());
+
+  for (int i = 0; i < S.size(); i++) {
+    count[Class[i]]++;
+  }
+
+  for (int i = 1; i < S.size(); i++) {
+    count[i] += count[i - 1];
+  }
+
+  for (int i = (S.size() - 1); i >= 0; i--) {
+    int start = (order[i] - L + S.size()) % S.size();
+    int cl = Class[start];
+    count[cl]--;
+    newOrder[count[cl]] = start;
+  }
+
+  return newOrder;
+}
+
+vector<int> UpdateClasses(const vector<int>& newOrder, vector<int>& Class, int L) {
+  int n = newOrder.size();
+  vector<int> newClass(n);
+  newClass[newOrder[0]] = 0;
+
+  for (int i = 1; i < n; i++) {
+    int cur = newOrder[i];
+    int prev = newOrder[i - 1];
+    int mid = cur + L;
+    int midPrev = (prev + L) % n;
+    if (Class[cur] != Class[prev] || Class[mid] != Class[midPrev]) {
+      newClass[cur] = newClass[prev] + 1;
+    } else {
+      newClass[cur] = newClass[prev];
+    }
+  }
+
+  return newClass;
+}
+
 vector<int> BuildSuffixArray(const string& text) {
-  vector<int> result;
+  //vector<int> result;
+  vector<int> order;
+  vector<int> Class;
+
   // Implement this function yourself
-  return result;
+  SortCharacters(text, order);
+
+#if DEBUG
+  cout << " == Printing Order == " << endl;
+  for (auto& o : order) {
+    cout << o << " ";
+  }
+  cout << endl;
+#endif
+
+  ComputeClassOrder(text, order, Class);
+
+#if DEBUG
+  cout << " == Printing Class == " << endl;
+  for (auto& o : Class) {
+    cout << o << " ";
+  }
+  cout << endl;
+#endif
+
+  int L = 1;
+
+  while (L < text.size()) {
+#if DEBUG
+    cout << "L = " << L << endl;
+#endif
+    vector<int> newOrder = SortDoubled(text, L, order, Class);
+    order = newOrder;
+#if DEBUG
+    cout << " == Printing newOrder == " << endl;
+    for (auto& o : order) {
+      cout << o << " ";
+    }
+    cout << endl;
+#endif
+    vector<int> newClass = UpdateClasses(order, Class, L);
+    Class = newClass;
+#if DEBUG
+    cout << " == Printing newClass == " << endl;
+    for (auto& o : Class) {
+      cout << o << " ";
+    }
+    cout << endl;
+#endif
+    L *= 2;
+  }
+
+  return order;
 }
 
 int main() {
