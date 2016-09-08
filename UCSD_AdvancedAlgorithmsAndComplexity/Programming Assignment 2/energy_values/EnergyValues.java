0447d13afec84396c472d4b4428db5306fe5180c
Stephen Liang <stphnliang@gmail.com>
2016-09-07 23:18:07 -0700

    stubs

--- UCSD_AdvancedAlgorithmsAndComplexity/Programming Assignment 2/energy_values/EnergyValues.java
+++ UCSD_AdvancedAlgorithmsAndComplexity/Programming Assignment 2/energy_values/EnergyValues.java
@@ -1,8 +1,19 @@
-class EnergyValues {
+// to compile
+// 				javac -encoding UTF-8 EnergyValues.java
+// to run from current directory
+// 				java -cp ./ EnergyValues
 
-    public static void main(String[] args) throws IOException {
-        Equation equation = ReadEquation();
-        double[] solution = SolveEquation(equation);
-        PrintColumn(solution);
-    }
+import java.io.*;
+
+public class EnergyValues {
+	public static void main (String[] args) {
+		try {
+			System.out.println("Hello, World");
+			int n = System.in.read();
+			System.out.format("%d", n);
+		} catch(IOException e){
+			System.out.print("Exception");
+		} finally {
+		}
+	}
 }
