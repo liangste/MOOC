4f1e63af77ab1a8c434e72e00754dc046dfaad31
Stephen Liang <stphnliang@gmail.com>
2016-10-02 20:03:05 -0700

    Hamiltonian cycle try 1

--- UCSD_AdvancedAlgorithmsAndComplexity/Programming Assignment 3/cleaning_apartment/cleaning_apartment.cpp
+++ UCSD_AdvancedAlgorithmsAndComplexity/Programming Assignment 3/cleaning_apartment/cleaning_apartment.cpp
@@ -1,42 +1,100 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-struct Edge {
-    int from;
-    int to;
+const vector<int> colors = {0, 1, 2};
+
+// x_ij
+// i == position in Hamiltonian path
+// j == node index
+
+int GetVarNum(int posIdx, int nodeIdx, int N) {
+	return N * posIdx + nodeIdx + 1;
 };
 
-struct ConvertHampathToSat {
-    int numVertices;
-    vector<Edge> edges;
+void PrintCNF(const vector<vector<bool> >& adjMatrix) {
+	int N = adjMatrix.size();
+	int nVars = N * N;
+	vector<vector<int> > clauses; // without 0 at end
 
-    ConvertHampathToSat(int n, int m) :
-        numVertices(n),
-        edges(m)
-    {  }
+	// each node must appear in the path
+	for (int j = 0; j < N; j++) {
+		vector<int> clause;
+		clause.clear();
+		for (int i = 0; i < N; i++) {
+			clause.push_back(GetVarNum(i, j, N));
+		}
+		clauses.push_back(clause);
+	}
 
-    void printEquisatisfiableSatFormula() {
-        // This solution prints a simple satisfiable formula
-        // and passes about half of the tests.
-        // Change this function to solve the problem.
-        cout << "3 2" << endl;
-        cout << "1 2 0" << endl;
-        cout << "-1 -2 0" << endl;
-        cout << "1 -2 0" << endl;
-    }
-};
+	// no node j appears twice in the path
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				if (i != k) {
+					clauses.push_back({-GetVarNum(i, j, N), -GetVarNum(k, j, N)});
+				}
+			}
+		}
+	}
 
-int main() {
-    ios::sync_with_stdio(false);
+	// every position i on the path must be occupied
+	for (int i = 0; i < N; i++) {
+		vector<int> clause;
+		clause.clear();
+		for (int j = 0; j < N; j++) {
+			clause.push_back(GetVarNum(i, j, N));
+		}
+		clauses.push_back(clause);
+	}
 
-    int n, m;
-    cin >> n >> m;
-    ConvertHampathToSat converter(n, m);
-    for (int i = 0; i < m; ++i) {
-        cin >> converter.edges[i].from >> converter.edges[i].to;
-    }
+	// no two noes j and k occupy the same position in the path
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				if (j != k) {
+					clauses.push_back({-GetVarNum(i, j, N), -GetVarNum(i, k, N)});
+				}
+			}
+		}
+	}
 
-    converter.printEquisatisfiableSatFormula();
+	// nonadjacent nodes i and j cannot be adjacent in the path
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < (N - 1); k++) {
+				if (!adjMatrix[i][j]) {
+					clauses.push_back({-GetVarNum(k, i, N), -GetVarNum(k + 1, j, N)});
+				}
+				if (!adjMatrix[j][i]) {
+					clauses.push_back({-GetVarNum(k, i, N), -GetVarNum(k + 1, j, N)});
+				}
+			}
+		}
+	}
 
-    return 0;
+	cout << clauses.size() << " " << nVars << endl;
+	for (int i = 0; i < clauses.size(); i++) {
+		for (int j = 0; j < clauses[i].size(); j++) {
+			cout << clauses[i][j] << " ";
+		}
+		cout << "0" << endl;
+	}
 }
+
+int main(void) {
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<bool> > adjMatrix(n, vector<bool>(n, false));
+
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		adjMatrix[a - 1][b - 1] = true;
+		adjMatrix[b - 1][a - 1] = true;
+	}
+
+	PrintCNF(adjMatrix);
+};
