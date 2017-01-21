5d36498603461967c55c3430fefbeb43de24434d
Stephen Liang <stphnliang@gmail.com>
2017-01-21 13:54:43 -0800

    Re-wrote code, now produces optimial hamiltonian path (travelling salesman path)

--- UCSD_GenomeSequencing/ps01/assembly-x174-error-free.cpp
+++ UCSD_GenomeSequencing/ps01/assembly-x174-error-free.cpp
@@ -1,27 +1,27 @@
 // Dataset Problem 1 : Assembling the phi X174 Genome from Error-Free Reads Using
 // Overlap Graphs
 
+#include <algorithm>
+#include <cstring>
 #include <iostream>
+#include <numeric>
 #include <set>
 #include <string>
 #include <vector>
 
-//#define DEBUG
+#define DEBUG
+//#define DEBUG2
 
 using namespace std;
 
-// represents an overlap between 2 reads from the gloabl Reads vector
-struct OverlapEdge {
-  int source;
-  int dest;
-  int weight;
-  // edge value is simply reads[dest].substr(0, weight)
-};
-
-typedef vector<vector<OverlapEdge>> vvOverlapEdge;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 
 vector<string> Reads;
-vvOverlapEdge AdjList;
+// weignt from source->dest, 0 if no edge
+// i.e. AdjMatrix[0][1] contains edge weight from 0'th read to 1st read
+char * AdjMatrix;
+#define MATRIX_VALUE(row, col) (AdjMatrix[row + col * Reads.size()])
 
 // get max length of suffix of suffixSource that match prefix of prefixSource
 // returns 0 if no such suffix-prefix match is found between the two inputs
@@ -40,7 +40,7 @@ void BuildOverlapGraph() {
       if (i != j) {
         int m = GetPrefixSuffixMatch(Reads[i], Reads[j]);
         if (m > 0) {
-          AdjList[i].push_back({i, j, m});
+          MATRIX_VALUE(i, j) = m;
         }
       }
     }
@@ -52,101 +52,114 @@ void DumpOverlapGraph() {
     cout << (i) << " " << Reads[i] << endl;
   }
 
-  for (int i = 0; i < AdjList.size(); i++) {
-    for (int j = 0; j < AdjList[i].size(); j++) {
-      cout << AdjList[i][j].source << "->" << AdjList[i][j].dest << " w=" << AdjList[i][j].weight << endl;
+  for (int i = 0; i < Reads.size(); i++) {
+    for (int j = 0; j < Reads.size(); j++) {
+      cout << (int) MATRIX_VALUE(i, j) << " ";
     }
+    cout << endl;
   }
 }
 
-bool GetNextLargestUnvisitedEdge(vector<bool>& visited, OverlapEdge& nextEdge) {
-  // TODO work on this
-  bool found;
-  for (int i = 0; i < AdjList.size(); i++) {
-    for (int j = 0; j < AdjList[i].size(); j++) {
-      if (!visited[AdjList[i][j].source] || !visited[AdjList[i][j].dest]) {
-        if (found) {
-          if (AdjList[i][j].weight > nextEdge.weight) {
-            nextEdge.source = AdjList[i][j].source;
-            nextEdge.dest = AdjList[i][j].dest;
-            nextEdge.weight = AdjList[i][j].weight;
-            found = true;
-          }
-        } else {
-          nextEdge.source = AdjList[i][j].source;
-          nextEdge.dest = AdjList[i][j].dest;
-          nextEdge.weight = AdjList[i][j].weight;
-          found = true;
-        }
-      }
-    }
-  }
-  return found;
+// gen permutation of vertices numbers from 0 to numVertices - 1
+void GenPermutationPaths(vvi& paths, int numVertices) {
+  vector<int> basePath(numVertices);
+  std::iota(basePath.begin(), basePath.end(), 0);
+  do {
+    paths.push_back(basePath);
+  } while (next_permutation(basePath.begin(), basePath.end()));
 }
 
-string DoTraversal(vector<OverlapEdge>& edges) {
-  // TODO work on this
-  vector<int> sourceIndices(edges.size());
-  vector<int> destIndices(edges.size());
-  vector<int> sourceWorthy(edges.size(), true);
-  for (int i = 0; i < edges.size(); i++) {
-    sourceIndices[edges[i].source] = i;
-    destIndices[edges[i].dest] = i;
-    sourceWorthy[edges[i].dest] = false;
-  }
+// return -1 if path is not a valid path in the overlap graph
+// otherwise returns sum of edges on this path
+int GetOverlapPathValue(vi& path) {
+  if (path.size() == 0) return -1;
 
-  int originalSource;
-  int theSource;
-  for (int i = 0; i < edges.size(); i++) {
-    if (sourceWorthy[i]) {
-      theSource = i;
-      break;
+  int cur = path[0];
+  int edgeSum = 0;
+  for (int i = 1; i < path.size(); i++) {
+    // TODO use adjacency matrix instead of edge
+    if (MATRIX_VALUE(cur, path[i]) > 0) {
+      edgeSum += MATRIX_VALUE(cur, path[i]);
+      cur = path[i];
+    } else {
+      return -1;
     }
   }
 
-  originalSource = theSource;
+  return edgeSum;
+}
 
-  string ret = Reads[theSource];
-  for (int i = 0; i < edges.size(); i++) {
-    int w = edges[sourceIndices[theSource]].weight;
-    int d = edges[sourceIndices[theSource]].dest;
-    ret += Reads[d].substr(w);
-    theSource = edges[sourceIndices[theSource]].dest;
+void PrintVectorInt(vector<int>& v) {
+  for (int j = 0; j < v.size(); j++) {
+    cout << v[j] << " ";
+  }
+  cout << endl;
+}
+
+string ReassembleGenomeByPath(vector<int>& path) {
+  if (path.size() == 0) return "";
+  int cur = path[0];
+  string ret = Reads[cur];
+
+  for (int i = 1; i < path.size(); i++) {
+    ret += Reads[path[i]].substr(MATRIX_VALUE(cur, path[i]));
+    cur = path[i];
   }
 
-  // if the last Read loops back to the first read, remove its edge from ret string
-  int lastRead = edges[sourceIndices[theSource]].dest;
-  for (int i = 0; i < AdjList[lastRead].size(); i++) {
-    if (AdjList[lastRead][i].dest == originalSource) {
-      int trimAmount = AdjList[lastRead][i].weight;
-      ret = ret.substr(0, ret.size() - trimAmount);
-      break;
-    }
+  // if last vertex links back to the first
+  int trim_value = MATRIX_VALUE(path[path.size() - 1], path[0]);
+  if (trim_value) {
+    ret = ret.substr(0, ret.size() - trim_value);
   }
 
   return ret;
 }
 
+// Hamiltonian Path is a path in a graph that traverses all vertices. But depending
+// on the starting vertex, a greedy path may not encounter all vertices.
+// This can happen if 1) starting vertex is wrong) or 2) at any given point
+// when making a decision, we encounter multiple edges with same weight
+// https://www.hackerearth.com/practice/algorithms/graphs/hamiltonian-path/tutorial/
 string DoGreedyHamiltonian() {
-  vector<bool> visited(Reads.size(), false);
-  vector<OverlapEdge> HamiltonianEdges;
-  while(true) {
-    OverlapEdge nextEdge;
-    bool hasNextEdge = GetNextLargestUnvisitedEdge(visited, nextEdge);
-    if (hasNextEdge) {
-      HamiltonianEdges.push_back(nextEdge);
-      visited[nextEdge.source] = true;
-      visited[nextEdge.dest] = true;
-    } else {
-      break;
+  // 1. generate permutations
+  // for each permutation, check wehther path is valid
+  // return path with largest sum edge weights
+  vvi paths;
+  GenPermutationPaths(paths, Reads.size());
+
+#ifdef DEBUG2
+  cout << "generated " << paths.size() << " permutation paths" << endl;
+
+  for (int i = 0; i < paths.size(); i++) {
+    PrintVectorInt(paths[i]);
+  }
+#endif
+
+  int m;
+  vector<int> bestPath;
+  int bestPathWeight = 0;
+
+  for (int i = 0; i < paths.size(); i++) {
+    m = GetOverlapPathValue(paths[i]);
+    if (m > 0) {
+      if (m > bestPathWeight) {
+        bestPathWeight = m;
+        bestPath = paths[i];
+      }
     }
   }
-  return DoTraversal(HamiltonianEdges);
+
+#ifdef DEBUG
+  cout << "best path has value " << bestPathWeight << endl;
+  PrintVectorInt(bestPath);
+#endif
+
+  return ReassembleGenomeByPath(bestPath);
 }
 
 int main(void) {
   string read;
-  set<string> readSet;
+  set<string> readSet; // remove duplicate reads
   while(cin >> read)
   {
     if (readSet.find(read) == readSet.end()) {
@@ -155,13 +168,16 @@ int main(void) {
     }
   }
 
-  AdjList.resize(Reads.size() + 1);
+  AdjMatrix = new char[Reads.size() * Reads.size()];
+  memset(AdjMatrix, 0, Reads.size() * Reads.size());
 
   BuildOverlapGraph();
 #ifdef DEBUG
   DumpOverlapGraph();
 #endif
   cout << DoGreedyHamiltonian() << endl;
+
+  delete[] AdjMatrix;
 
   return 0;
 }
