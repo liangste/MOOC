6ef3792d7f8eef113088e6a7818ec068a0e4c56d
Stephen Liang <stphnlaing@gmail.com>
2017-02-04 17:07:44 -0800

    super-fast k-universal circular string construction using De Bruijn Graph

--- UCSD_GenomeSequencing/ps02/k_universal_circular_string/kucs.cpp
+++ UCSD_GenomeSequencing/ps02/k_universal_circular_string/kucs.cpp
@@ -1,28 +1,50 @@
-// refs : http://www.graph-magics.com/articles/euler.php
-
 #include <algorithm>
 #include <iostream>
 #include <stack>
 #include <unordered_map>
 #include <vector>
 
-//#define DEBUG1
-#define LOOP(x, m) for(int x = 0; x < m; x++)
-
 using namespace std;
+
+// #define DEBUG
+#define LOOP(i, m) for(int i = 0; i < m; i++)
+
+typedef vector<vector<int>> vvi;
 
 class Graph {
 public:
-  // n : number of vertices, vertices are 1-indexed
-  Graph(int n)
-    : m_vertices(n)
-    , m_adjList(n)
-    , m_inDegrees(n) {
-  }
+  // K : k-mer for the De Bruijin Graph
+  // NOTE that all edges are simply 0 -> (1 << K)
+  // and all vertices are 0 -> (1 << (K - 1))
+  Graph(uint32_t K) : m_k(K) {
+    m_edges = (1 << m_k);
+    m_vertices = m_edges >> 1;
+    m_edgeMask = m_edges - 1;
+    m_vertexMask = m_vertices - 1;
+#ifdef DEBUG
+    printf("edge mask = 0x%04x\n", m_edgeMask);
+    printf("vertex mask = 0x%04x\n", m_vertexMask);
+    cout << "Number of edges = " << m_edges << " and number of vertices = " << m_vertices << endl;
+#endif
+    m_adjList.resize(m_vertices);
+    m_inDegrees.resize(m_vertices);
+  };
 
-  void AddEdge(int f, int t) {
-    m_adjList[f - 1].push_back(t - 1);
-    m_inDegrees[t - 1]++;
+  // each vertex with pattern [a_0 .. a_n] can only connect to vertices
+  // [a_1 .. a_n 0] or [a_1 .. a_n 1]
+  void BuildDeBruijnGraph() {
+    LOOP(i, m_vertices) {
+      uint32_t to_0, to_1;
+      to_0 = (i << 1) & m_vertexMask;
+      to_1 = (i << 1) & m_vertexMask | 1;
+#ifdef DEBUG
+      cout << i << " -> " << to_0 << " and " << to_1 << endl;
+#endif
+      m_adjList[i].push_back(to_0);
+      m_adjList[i].push_back(to_1);
+      m_inDegrees[to_0]++;
+      m_inDegrees[to_1]++;
+    }
   }
 
   // check for Eulerian cycle conditions
@@ -88,10 +110,13 @@ public:
     // we've got the cycle in reverse
     reverse(circuit.begin(), circuit.end());
     circuit.pop_back();
-    for_each(circuit.begin(), circuit.end(), [](int& i) {i++;});
   }
 
-private:
+  //
+  bool Overlaps_(uint32_t suffix, uint32_t prefix) {
+    return false;
+  }
+
   void Explore_(vector<vector<int>>& adj, vector<int>& used, int x) {
   	used[x] = 1;
     m_sccNums[x] = m_curSccNum;
@@ -144,36 +169,59 @@ private:
 #endif
   }
 
-  int                       m_vertices;
-  vector<vector<int>>       m_adjList;
-  vector<int>               m_inDegrees;
-  int                       m_curSccNum;
-  vector<int>               m_sccNums;
+  uint32_t          m_k;
+  uint32_t          m_edgeMask;
+  uint32_t          m_vertexMask;
+  uint32_t          m_edges;
+  uint32_t          m_vertices;
+  vvi               m_adjList;
+  vector<int>       m_inDegrees;
+  int               m_curSccNum;
+  vector<int>       m_sccNums;
 };
 
 int main(void) {
-  int V, E;
-  cin >> V >> E;
-  Graph testGraph(V);
-  int f, t;
-  while (E--) {
-    cin >> f >> t;
-    testGraph.AddEdge(f, t);
-  }
+  uint32_t K;
+  cin >> K;
+  Graph testGraph(K);
+  testGraph.BuildDeBruijnGraph();
+  if(testGraph.CheckEulerian()) {
+#ifdef DEBUG
+    cout << "Eulerian cycle exists" << endl;
+#endif
 
-  if (testGraph.CheckEulerian()) {
-    cout << "1" << endl;
     vector<int> eulerianPath;
-
     // get the eulerian cycle
     testGraph.GetEulerianCycle(eulerianPath);
 
+    if (eulerianPath.size() == 0) return 2;
+
+#ifdef DEBUG
     // print it out
     for_each(eulerianPath.begin(), eulerianPath.end(), [](int& i){cout << i << " ";});
     cout << endl;
-  } else {
-    cout << "0" << endl;
+#endif
+
+    string result;
+    uint32_t idx = 1 << (K - 1);
+
+    for (int i = idx; idx > 1; idx = idx >> 1) {
+      result += (eulerianPath[0] & idx)? "1" : "0";
+    }
+
+    for (int i = 1; i < eulerianPath.size(); i++) {
+      result += (eulerianPath[i] & 1)? "1" : "0";
+    }
+
+    if (eulerianPath[0] == eulerianPath[eulerianPath.size() - 1]) {
+      // trim back
+      result = result.substr(0, result.size() - (K - 2));
+    }
+
+    cout << result << endl;
+
+    return 0;
   }
 
-  return 0;
+  return 1;
 }
